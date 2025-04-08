7d4c3f4689605cfa4b4a89e77a1e943d6606e564
Mark Harrison <mark.harrison@students.snow.edu>
2025-04-08 08:34:56 -0600

    name and maze paths finished

--- Main.cpp
+++ Main.cpp
@@ -12,8 +12,8 @@
 const std::string kYourName = "Mark Harrison";
 
 /* Change these constants to contain the paths out of your mazes. */
-const std::string kPathOutOfNormalMaze = "TODO: Replace this string with your path out of the normal maze.";
-const std::string kPathOutOfTwistyMaze = "TODO: Replace this string with your path out of the twisty maze.";
+const std::string kPathOutOfNormalMaze = "EESSSNNNWWWSSSEEWNNES";
+const std::string kPathOutOfTwistyMaze = "SSNNESWNWS";
 
 int main() {
     /* Generate the maze.
